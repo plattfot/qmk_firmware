7dcf767fb413b1d2ea43b223295eae2dd04af83e
Fredrik Salomonsson <plattfot@gmail.com>
2020-03-15 18:20:16 -0700

    [Keymap] plattfot - Changed Hyper and Insert keys to parenthesis

--- layouts/community/ergodox/plattfot/keymap.c
+++ layouts/community/ergodox/plattfot/keymap.c
@@ -29,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ~    |   1  |   2  |   3  |   4  |   5  | ECS  |           | Meh  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   ~    |   1  |   2  |   3  |   4  |   5  | ECS  |           | ESC  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   \    |   Q  |   W  |   E  |   R  |   T  | ^L2  |           |  =   |   Y  |   U  |   I  |   O  |   P  |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| Hyper|           |Insert|------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  (   |           |  )   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           '-------------+------+------+------+------+--------'
  *   | ~L4  |   ]  |  </> | LGUI | LCTL |                                       |  L3  | C-M  | RGUI | ~L1  | ~L4  |
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | Del  |ScLock|       | PgUp |  PgDn  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | End  |        |      |
- *                                 | Space| LALT |------|       |------| ENTER  | Back |
+ *                                 | Space| LALT |------|       |------| Enter  | Back |
  *                                 |      |      | L2   |       | RALT |        | Space|
  *                                 `--------------------'       `----------------------'
  */
@@ -54,16 +54,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_ESC,
         KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    OSL(SYMB),
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    ALL_T(KC_NO),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_LPRN,
         TG(MOUS),KC_RBRC, KC_NUBS, KC_LGUI, KC_LCTL,
                                                     KC_DEL,  KC_SLCK,
                                                              KC_HOME,
                                             KC_SPC, KC_LALT, MO(SYMB),
         // right hand
-        KC_ENT,      KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_ESC,      KC_6,   KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_EQL,      KC_Y,   KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC,
                      KC_H,   KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_INS,      KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_RPRN,     KC_N,   KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                              MO(MDIA),  LALT(KC_RCTL), KC_RGUI, TG(GAME),TG(MOUS),
         KC_PGUP,     KC_PGDN,
         KC_END,
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, KC_LPRN, KC_RPRN, KC_BSLS, KC_MINS, M_LRABR,
        _______, _______, M_LRBRC, M_LRPRN, M_LRCBR, _______, _______,
        _______, _______, _______, _______, _______,
-                                                    _______, _______,
+                                                    _______, KC_INS,
                                                              _______,
                                            _______, _______, _______,
        // right hand
--- layouts/community/ergodox/plattfot/readme.md
+++ layouts/community/ergodox/plattfot/readme.md
@@ -5,34 +5,13 @@ thumb cluster or where the thumb can reach. Geared towards programming;
 "()\- {}[]" is on the home row for example. Layed out to support
 typing in both english and swedish.
 
-## Changelog
-* v4 [Nov 2017]: 
-  * Fn changed to one shot layer key
-  * Right hand got key for Ctrl+Alt
-* v3 [Nov 2017]:
-  * Control backlight on each hand
-  * Right hand switched toggle media to toggle mouse/backlight
-* v2 [Nov 2017]:
-  * LCD prints current layer name.
-  * LCD using constant color backlight
-* v1 [Sep 2017]:
-  * Updated layout to ergodox infinity
-  * Added print screen on the media layer for use as sysreq. See [FAQ](https://docs.qmk.fm/faq_keymap.html#kcsysreq-isnt-working).
-  * Updated macros to use SEND_STRING and process_record_user
-* v0 [Feb 2017]:
-  * Ported configuration from my ergodox-firmware fork
-  * Changed space to work as space when tapped and ctrl when hold, same for enter except alt when hold.
-  * Added game layer that disables space/ctrl and enter/alt
-  * Added macros to insert <> and ""
-  * Media layer also can move the mouse
-  
-Main layer
+Main layer (0)
 ![Default](https://imgur.com/aOYpYUR.png)
-Game layer
+Game layer (1)
 ![Game](https://imgur.com/zL3Dacx.png)
-Symbol layer
+Symbol layer (2)
 ![Symbol](https://imgur.com/PvJxB4x.png)
-Media layer
-![Symbol](https://imgur.com/2eRTYis.png)
-Mouse layer
-![Symbol](https://imgur.com/t9g3w6g.png)
+Media layer (3)
+![Media](https://imgur.com/2eRTYis.png)
+Mouse layer (4)
+![Mouse](https://imgur.com/t9g3w6g.png)
