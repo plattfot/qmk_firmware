a010ca1a7368fbbb6a86a6bc55b2d1b760acabac
Fredrik Salomonsson <plattfot@posteo.net>
2022-01-10 21:41:45 -0800

    plattfot - Disable inner most thumb keys
    Not comfortable to use those keys so I'm just going to disable them.

--- keyboards/splitkb/kyria/keymaps/plattfot/keymap.c
+++ keyboards/splitkb/kyria/keymaps/plattfot/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      | RAISE|  | LOWER|      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `----------------------+------+------+------| Space|------|  |------| Enter|------+------+------+----------------------'
- *                        | MPlay| AltGr| LGUI |      | LCtrl|  | LAlt |      | RGUI | AltGr| Mic  |
+ *                        | MPlay|      | LGUI |      | LCtrl|  | LAlt |      | RGUI |      | Mic  |
  *                        |      |      |      |      |      |  |      |      |      |      | Mute |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_DEL,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
       KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    XXXXXXX,  L_RAISE, R_LOWER, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_MPLY,KC_RALT,KC_LGUI, KC_SPACE, KC_LCTL, KC_LALT,  KC_ENT, KC_RGUI, KC_RALT, KC_F20
+                               KC_MPLY,XXXXXXX,KC_LGUI, KC_SPACE, KC_LCTL, KC_LALT,  KC_ENT, KC_RGUI, XXXXXXX, KC_F20
     ),
 
 /*
