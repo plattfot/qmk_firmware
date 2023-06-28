447b65c829d708fd09d102d57bbc155e93bc23e1
Fredrik Salomonsson <plattfot@posteo.net>
2023-06-28 10:29:22 -0700

    [plattfot] keymap - Turn off num_lock and reorder in C_CLEAR
    It wasn't clearing the mouse layer properly as the led_update_user
    would turn it back on.  Also moving clear_oneshot_mods() to be before
    the others as otherwise it seems I need to double tap on clear for it
    to properly clear them.

--- keyboards/ferris/keymaps/plattfot/keymap.c
+++ keyboards/ferris/keymaps/plattfot/keymap.c
@@ -325,10 +325,13 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         break;
     case C_CLEAR:
         if (pressed) {
+            if (host_keyboard_led_state().num_lock) {
+                tap_code(KC_NUM_LOCK);
+            }
+            clear_oneshot_mods();
             layer_move(_BASE);
             caps_word_set(false);
             cancel_close_tap();
-            clear_oneshot_mods();
         }
         break;
     case C_CAPS:
