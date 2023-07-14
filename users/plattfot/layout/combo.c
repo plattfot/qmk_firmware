9048bda0589d5c433965aad7ff252c20b9a998a7
Fredrik Salomonsson <plattfot@posteo.net>
2023-07-14 10:33:10 -0700

    [user] plattfot - rename PLT_ENABLE_COMBO40 to PLT_ENABLE_COMBO_FULL
    And promote the upper combos to be included by default + the del and
    backspace on the lower as they are easy to press on the kyria as well.

--- users/plattfot/layout/combo.c
+++ users/plattfot/layout/combo.c
@@ -17,30 +17,40 @@ enum combo_events {
 #ifdef PLT_ENABLE_CAPS_WORD
   C_CAPS, // caps word
 #endif
-#ifdef PLT_ENABLE_COMBO40
+  // Upper left hand
   C_CLEAR, // clear state
   C_LALTL,
   C_LCTRL,
   C_LGUI,
-  C_LSHIFT,
-
-  C_CPU, // Ctri + PgUp
-  C_DEL,
-  C_MPLY,
-  C_RALTL,
   C_TAB,
 
+  // Lower left hand
+  C_DEL,
+
+  // Upper right hand
   C_RSHORT,
   C_LALTR,
   C_RCTRL,
   C_RGUI,
-  C_RSHIFT,
+  C_NAV, // to navigation
 
-  C_CPD, // Ctri + PgDn
+  // Lower right hand
   C_BSPC,
+
+#ifdef PLT_ENABLE_COMBO_FULL
+  // Lower left hand
+  C_CPU, // Ctri + PgUp
+  // C_DEL placement
+  C_MPLY,
+  C_RALTL,
+  C_LSHIFT,
+
+  // Lower right hand
+  C_CPD, // Ctri + PgDn
+  // C_BSPC placement
   C_F20, // mic mute
   C_RALTR,
-  C_NAV, // to navigation
+  C_RSHIFT,
 #endif
 
   COMBO_LENGTH
@@ -52,18 +62,13 @@ const uint16_t PROGMEM esc_combo[] = {KC_SPACE, KC_ENT, COMBO_END};
 const uint16_t PROGMEM caps_combo[] = {KC_G, KC_M, COMBO_END};
 #endif
 
-#ifdef PLT_ENABLE_COMBO40
 const uint16_t PROGMEM clear_combo[] = {KC_B, KC_G, COMBO_END};
 const uint16_t PROGMEM laltl_combo[] = {KC_P, KC_T, COMBO_END};
 const uint16_t PROGMEM lctrl_combo[] = {KC_F, KC_S, COMBO_END};
 const uint16_t PROGMEM lgui_combo[] = {KC_W, KC_R, COMBO_END};
 const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_A, COMBO_END};
 
-const uint16_t PROGMEM cpu_combo[] = {KC_G, KC_V, COMBO_END};
 const uint16_t PROGMEM del_combo[] = {KC_T, KC_D, COMBO_END};
-const uint16_t PROGMEM mply_combo[] = {KC_S, KC_C, COMBO_END};
-const uint16_t PROGMEM raltl_combo[] = {KC_R, KC_X, COMBO_END};
-const uint16_t PROGMEM lshift_combo[] = {KC_A, KC_Z, COMBO_END};
 
 const uint16_t PROGMEM rshort_combo[] = {KC_J, KC_M, COMBO_END};
 const uint16_t PROGMEM laltr_combo[] = {KC_L, KC_N, COMBO_END};
@@ -71,8 +76,17 @@ const uint16_t PROGMEM rctrl_combo[] = {KC_U, KC_E, COMBO_END};
 const uint16_t PROGMEM rgui_combo[] = {KC_Y, KC_I, COMBO_END};
 const uint16_t PROGMEM nav_combo[] = {KC_SCLN, KC_O, COMBO_END};
 
-const uint16_t PROGMEM cpd_combo[] = {KC_M, KC_K, COMBO_END};
 const uint16_t PROGMEM bspc_combo[] = {KC_N, KC_H, COMBO_END};
+
+#ifdef PLT_ENABLE_COMBO_FULL
+const uint16_t PROGMEM cpu_combo[] = {KC_G, KC_V, COMBO_END};
+// del
+const uint16_t PROGMEM mply_combo[] = {KC_S, KC_C, COMBO_END};
+const uint16_t PROGMEM raltl_combo[] = {KC_R, KC_X, COMBO_END};
+const uint16_t PROGMEM lshift_combo[] = {KC_A, KC_Z, COMBO_END};
+
+const uint16_t PROGMEM cpd_combo[] = {KC_M, KC_K, COMBO_END};
+// backspace
 const uint16_t PROGMEM micmute_combo[] = {KC_E, KC_COMM, COMBO_END};
 const uint16_t PROGMEM raltr_combo[] = {KC_I, KC_DOT, COMBO_END};
 const uint16_t PROGMEM rshift_combo[] = {KC_O, KC_SLSH, COMBO_END};
@@ -84,37 +98,39 @@ combo_t key_combos[] = {
  [C_CAPS] = COMBO_ACTION(caps_combo),
 #endif
 
-#ifdef PLT_ENABLE_COMBO40
  [C_CLEAR] = COMBO_ACTION(clear_combo),
  [C_LALTL] = COMBO_ACTION(laltl_combo),
  [C_LCTRL] = COMBO_ACTION(lctrl_combo),
  [C_LGUI] = COMBO_ACTION(lgui_combo),
- [C_LSHIFT] = COMBO_ACTION(lshift_combo),
-
- [C_CPU] = COMBO(cpu_combo, LCTL(KC_PGUP)),
- [C_DEL] = COMBO(del_combo, KC_DEL),
- [C_MPLY] = COMBO(mply_combo, KC_MPLY),
- [C_RALTL] = COMBO_ACTION(raltl_combo),
  [C_TAB] = COMBO(tab_combo, KC_TAB),
+
+ [C_DEL] = COMBO(del_combo, KC_DEL),
 
  [C_RSHORT] = COMBO_ACTION(rshort_combo),
  [C_LALTR] = COMBO_ACTION(laltr_combo),
  [C_RCTRL] = COMBO_ACTION(rctrl_combo),
  [C_RGUI] = COMBO_ACTION(rgui_combo),
- [C_RSHIFT] = COMBO_ACTION(rshift_combo),
+ [C_NAV] = COMBO_ACTION(nav_combo),
+
+ [C_BSPC] = COMBO(bspc_combo, KC_BSPC),
+#ifdef PLT_ENABLE_COMBO_FULL
+ [C_CPU] = COMBO(cpu_combo, LCTL(KC_PGUP)),
+ // Del
+ [C_MPLY] = COMBO(mply_combo, KC_MPLY),
+ [C_RALTL] = COMBO_ACTION(raltl_combo),
+ [C_LSHIFT] = COMBO_ACTION(lshift_combo),
 
  [C_CPD] = COMBO(cpd_combo, RCTL(KC_PGDN)),
- [C_BSPC] = COMBO(bspc_combo, KC_BSPC),
+ // Backspace
  [C_F20] = COMBO(micmute_combo, KC_F20),
  [C_RALTR] = COMBO_ACTION(raltr_combo),
- [C_NAV] = COMBO_ACTION(nav_combo),
+ [C_RSHIFT] = COMBO_ACTION(rshift_combo),
 #endif
 };
 
 __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool pressed)
 {
     switch(combo_index) {
-#ifdef PLT_ENABLE_COMBO40
     case C_LCTRL:
         if (pressed) {
             register_code(KC_LCTL);
@@ -140,6 +156,7 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             unregister_code(KC_LALT);
         }
         break;
+#ifdef PLT_ENABLE_COMBO_FULL
     case C_RALTR:
     case C_RALTL:
         if (pressed) {
@@ -147,22 +164,6 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             add_oneshot_mods(MOD_BIT(KC_RALT));
         } else {
             unregister_code(KC_RALT);
-        }
-        break;
-    case C_LGUI:
-        if (pressed) {
-            register_code(KC_LGUI);
-            add_oneshot_mods(MOD_BIT(KC_LGUI));
-        } else {
-            unregister_code(KC_LGUI);
-        }
-        break;
-    case C_RGUI:
-        if (pressed) {
-            register_code(KC_RGUI);
-            add_oneshot_mods(MOD_BIT(KC_RGUI));
-        } else {
-            unregister_code(KC_RGUI);
         }
         break;
     case C_LSHIFT:
@@ -181,6 +182,23 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             unregister_code(KC_RSFT);
         }
         break;
+#endif
+    case C_LGUI:
+        if (pressed) {
+            register_code(KC_LGUI);
+            add_oneshot_mods(MOD_BIT(KC_LGUI));
+        } else {
+            unregister_code(KC_LGUI);
+        }
+        break;
+    case C_RGUI:
+        if (pressed) {
+            register_code(KC_RGUI);
+            add_oneshot_mods(MOD_BIT(KC_RGUI));
+        } else {
+            unregister_code(KC_RGUI);
+        }
+        break;
     case C_RSHORT:
         if (pressed) {
             set_oneshot_layer(plt_shortcut_index(), ONESHOT_START);
@@ -195,7 +213,6 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             clear_oneshot_layer_state(ONESHOT_PRESSED);
         }
         break;
-#endif
     case C_ESC:
         if (pressed) {
             tap_code16(KC_ESC);
@@ -215,11 +232,13 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
 #endif
         }
         break;
+#ifdef PLT_ENABLE_CAPS_WORD
     case C_CAPS:
         if (pressed) {
             caps_word_set(!caps_word_get());
         }
         break;
+#endif
     }
 }
 
