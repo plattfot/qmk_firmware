70460f59b68b4a8dde55de8568e6b90a54313d50
Fredrik Salomonsson <plattfot@posteo.net>
2023-07-14 09:31:46 -0700

    [keypmap] plattfot - update combo changes on kyria
    Need to disable rgb as everything cannot fit in the memory of the
    microcontroller.

--- keyboards/splitkb/kyria/keymaps/plattfot/config.h
+++ keyboards/splitkb/kyria/keymaps/plattfot/config.h
@@ -42,6 +42,8 @@
 // and time out caps word after 5s
 #define CAPS_WORD_IDLE_TIMEOUT 5000
 
+#define COMBO_ONLY_FROM_LAYER 0
+
 #define PLT_ENABLE_CAPS_WORD
 #define PLT_ENABLE_CLOSE_TAP
 // #define PLT_ENABLE_COMBO40
