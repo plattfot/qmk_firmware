f578506b8fed9f9f539290ff4f6e0241f2fa8211
Fredrik Salomonsson <plattfot@posteo.net>
2023-07-15 10:00:44 -0700

    [user] plattfot - Move del and backspace to full combo
    They didn't quite work with a mx layout as it was harder to hit the
    two keys.  The top row worked out better.

--- users/plattfot/layout/combo.c
+++ users/plattfot/layout/combo.c
@@ -24,9 +24,6 @@ enum combo_events {
   C_LGUI,
   C_TAB,
 
-  // Lower left hand
-  C_DEL,
-
   // Upper right hand
   C_RSHORT,
   C_LALTR,
@@ -34,20 +31,17 @@ enum combo_events {
   C_RGUI,
   C_NAV, // to navigation
 
-  // Lower right hand
-  C_BSPC,
-
 #ifdef PLT_ENABLE_COMBO_FULL
   // Lower left hand
   C_CPU, // Ctri + PgUp
-  // C_DEL placement
+  C_DEL,
   C_MPLY,
   C_RALTL,
   C_LSHIFT,
 
   // Lower right hand
   C_CPD, // Ctri + PgDn
-  // C_BSPC placement
+  C_BSPC,
   C_F20, // mic mute
   C_RALTR,
   C_RSHIFT,
@@ -68,25 +62,21 @@ const uint16_t PROGMEM lctrl_combo[] = {KC_F, KC_S, COMBO_END};
 const uint16_t PROGMEM lgui_combo[] = {KC_W, KC_R, COMBO_END};
 const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_A, COMBO_END};
 
-const uint16_t PROGMEM del_combo[] = {KC_T, KC_D, COMBO_END};
-
 const uint16_t PROGMEM rshort_combo[] = {KC_J, KC_M, COMBO_END};
 const uint16_t PROGMEM laltr_combo[] = {KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM rctrl_combo[] = {KC_U, KC_E, COMBO_END};
 const uint16_t PROGMEM rgui_combo[] = {KC_Y, KC_I, COMBO_END};
 const uint16_t PROGMEM nav_combo[] = {KC_SCLN, KC_O, COMBO_END};
 
-const uint16_t PROGMEM bspc_combo[] = {KC_N, KC_H, COMBO_END};
-
 #ifdef PLT_ENABLE_COMBO_FULL
 const uint16_t PROGMEM cpu_combo[] = {KC_G, KC_V, COMBO_END};
-// del
+const uint16_t PROGMEM del_combo[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM mply_combo[] = {KC_S, KC_C, COMBO_END};
 const uint16_t PROGMEM raltl_combo[] = {KC_R, KC_X, COMBO_END};
 const uint16_t PROGMEM lshift_combo[] = {KC_A, KC_Z, COMBO_END};
 
 const uint16_t PROGMEM cpd_combo[] = {KC_M, KC_K, COMBO_END};
-// backspace
+const uint16_t PROGMEM bspc_combo[] = {KC_N, KC_H, COMBO_END};
 const uint16_t PROGMEM micmute_combo[] = {KC_E, KC_COMM, COMBO_END};
 const uint16_t PROGMEM raltr_combo[] = {KC_I, KC_DOT, COMBO_END};
 const uint16_t PROGMEM rshift_combo[] = {KC_O, KC_SLSH, COMBO_END};
@@ -104,24 +94,21 @@ combo_t key_combos[] = {
  [C_LGUI] = COMBO_ACTION(lgui_combo),
  [C_TAB] = COMBO(tab_combo, KC_TAB),
 
- [C_DEL] = COMBO(del_combo, KC_DEL),
-
  [C_RSHORT] = COMBO_ACTION(rshort_combo),
  [C_LALTR] = COMBO_ACTION(laltr_combo),
  [C_RCTRL] = COMBO_ACTION(rctrl_combo),
  [C_RGUI] = COMBO_ACTION(rgui_combo),
  [C_NAV] = COMBO_ACTION(nav_combo),
 
- [C_BSPC] = COMBO(bspc_combo, KC_BSPC),
 #ifdef PLT_ENABLE_COMBO_FULL
  [C_CPU] = COMBO(cpu_combo, LCTL(KC_PGUP)),
- // Del
+ [C_DEL] = COMBO(del_combo, KC_DEL),
  [C_MPLY] = COMBO(mply_combo, KC_MPLY),
  [C_RALTL] = COMBO_ACTION(raltl_combo),
  [C_LSHIFT] = COMBO_ACTION(lshift_combo),
 
  [C_CPD] = COMBO(cpd_combo, RCTL(KC_PGDN)),
- // Backspace
+ [C_BSPC] = COMBO(bspc_combo, KC_BSPC),
  [C_F20] = COMBO(micmute_combo, KC_F20),
  [C_RALTR] = COMBO_ACTION(raltr_combo),
  [C_RSHIFT] = COMBO_ACTION(rshift_combo),
