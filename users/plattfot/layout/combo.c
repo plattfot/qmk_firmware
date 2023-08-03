644a7ddb8e76fab42881e80736b13397a2b80f52
Fredrik Salomonsson <plattfot@posteo.net>
2023-08-03 12:38:46 -0700

    [user] plattfot - Enable shift combos and shuffle the combos
    Switch the order of shift, altgr and gui.  Altgr is the least one I
    press, so that get moved to the lower row.  Having issue with my pinky
    and I think I'm using shift more than GUI so swapping the order of
    those.

--- users/plattfot/layout/combo.c
+++ users/plattfot/layout/combo.c
@@ -21,38 +21,40 @@ enum combo_events {
   C_CLEAR, // clear state
   C_LALTL,
   C_LCTRL,
+  C_LSHIFT,
   C_LGUI,
-  C_RALTL,
 
   // Upper right hand
   C_RSHORT,
   C_LALTR,
   C_RCTRL,
+  C_RSHIFT,
   C_RGUI,
-  C_RALTR,
 
   // Lower left hand
   C_TAB,
   C_LNAV, // to navigation
+  C_RALTL,
 
   // Lower right hand
   C_BSPC, // key will be del when on plt_left_symfunc_index layer
   C_RNAV, // to navigation
+  C_RALTR,
 
 #ifdef PLT_ENABLE_COMBO_FULL
   // Lower left hand
   C_CPU, // Ctri + PgUp
   /* C_TAB, */
   /* C_LNAV*/
+  /* C_RALTL, */
   C_MPLY,
-  C_LSHIFT,
 
   // Lower right hand
   C_CPD, // Ctri + PgDn
   /* C_BSPC, */
   /* C_RNAV*/
+  /* C_RALTR, */
   C_F20, // mic mute
-  C_RSHIFT,
 #endif
 
   COMBO_LENGTH
@@ -67,33 +69,35 @@ const uint16_t PROGMEM caps_combo[] = {KC_G, KC_M, COMBO_END};
 const uint16_t PROGMEM clear_combo[] = {KC_B, KC_G, COMBO_END};
 const uint16_t PROGMEM laltl_combo[] = {KC_P, KC_T, COMBO_END};
 const uint16_t PROGMEM lctrl_combo[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM lgui_combo[] = {KC_W, KC_R, COMBO_END};
-const uint16_t PROGMEM raltl_combo[] = {KC_Q, KC_A, COMBO_END};
+const uint16_t PROGMEM lshift_combo[] = {KC_W, KC_R, COMBO_END};
+const uint16_t PROGMEM lgui_combo[] = {KC_Q, KC_A, COMBO_END};
 
 const uint16_t PROGMEM rshort_combo[] = {KC_J, KC_M, COMBO_END};
 const uint16_t PROGMEM laltr_combo[] = {KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM rctrl_combo[] = {KC_U, KC_E, COMBO_END};
-const uint16_t PROGMEM rgui_combo[] = {KC_Y, KC_I, COMBO_END};
-const uint16_t PROGMEM raltr_combo[] ={KC_SCLN, KC_O, COMBO_END};
+const uint16_t PROGMEM rshift_combo[] = {KC_Y, KC_I, COMBO_END};
+const uint16_t PROGMEM rgui_combo[] = {KC_SCLN, KC_O, COMBO_END};
 
 const uint16_t PROGMEM tab_combo[] = {KC_T, KC_D, COMBO_END};
 const uint16_t PROGMEM lnav_combo[] = {KC_S, KC_C, COMBO_END};
+const uint16_t PROGMEM raltl_combo[] = {KC_R, KC_X, COMBO_END};
 
 const uint16_t PROGMEM bspc_combo[] = {KC_N, KC_H, COMBO_END};
 const uint16_t PROGMEM rnav_combo[] = {KC_E, KC_COMM, COMBO_END};
+const uint16_t PROGMEM raltr_combo[] ={KC_I, KC_DOT, COMBO_END};
 
 #ifdef PLT_ENABLE_COMBO_FULL
 const uint16_t PROGMEM cpu_combo[] = {KC_G, KC_V, COMBO_END};
 // <- tab placement ->
 // <- nav placement ->
-const uint16_t PROGMEM mply_combo[] = {KC_R, KC_X, COMBO_END};
-const uint16_t PROGMEM lshift_combo[] = {KC_A, KC_Z, COMBO_END};
+// <- altgr placement ->
+const uint16_t PROGMEM mply_combo[] = {KC_A, KC_Z, COMBO_END};
 
 const uint16_t PROGMEM cpd_combo[] = {KC_M, KC_K, COMBO_END};
 // <- backspace placement ->
 // <- nav placement ->
-const uint16_t PROGMEM micmute_combo[] = {KC_I, KC_DOT, COMBO_END};
-const uint16_t PROGMEM rshift_combo[] = {KC_O, KC_SLSH, COMBO_END};
+// <- altgr placement ->
+const uint16_t PROGMEM micmute_combo[] = {KC_O, KC_SLSH, COMBO_END};
 #endif
 
 combo_t key_combos[] = {
@@ -105,33 +109,35 @@ combo_t key_combos[] = {
  [C_CLEAR] = COMBO_ACTION(clear_combo),
  [C_LALTL] = COMBO_ACTION(laltl_combo),
  [C_LCTRL] = COMBO_ACTION(lctrl_combo),
+ [C_LSHIFT] = COMBO_ACTION(lshift_combo),
  [C_LGUI] = COMBO_ACTION(lgui_combo),
- [C_RALTL] = COMBO_ACTION(raltl_combo),
 
  [C_RSHORT] = COMBO_ACTION(rshort_combo),
  [C_LALTR] = COMBO_ACTION(laltr_combo),
  [C_RCTRL] = COMBO_ACTION(rctrl_combo),
+ [C_RSHIFT] = COMBO_ACTION(rshift_combo),
  [C_RGUI] = COMBO_ACTION(rgui_combo),
- [C_RALTR] = COMBO_ACTION(raltr_combo),
 
  [C_TAB] = COMBO(tab_combo, KC_TAB),
  [C_LNAV] = COMBO_ACTION(lnav_combo),
+ [C_RALTL] = COMBO_ACTION(raltl_combo),
 
  [C_BSPC] = COMBO_ACTION(bspc_combo),
  [C_RNAV] = COMBO_ACTION(rnav_combo),
+ [C_RALTR] = COMBO_ACTION(raltr_combo),
 
 #ifdef PLT_ENABLE_COMBO_FULL
  [C_CPU] = COMBO(cpu_combo, LCTL(KC_PGUP)),
  // <- tab ->
  // <- nav ->
+ // <- altgr ->
  [C_MPLY] = COMBO(mply_combo, KC_MPLY),
- [C_LSHIFT] = COMBO_ACTION(lshift_combo),
 
  [C_CPD] = COMBO(cpd_combo, RCTL(KC_PGDN)),
  // <- backspace ->
  // <- nav ->
+ // <- altgr ->
  [C_F20] = COMBO(micmute_combo, KC_F20),
- [C_RSHIFT] = COMBO_ACTION(rshift_combo),
 #endif
 };
 
@@ -146,7 +152,6 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             clear_oneshot_layer_state(ONESHOT_PRESSED);
         }
         break;
-#ifdef PLT_ENABLE_COMBO_FULL
     case C_LSHIFT:
         if (pressed) {
             register_code(KC_LSFT);
@@ -163,7 +168,6 @@ __attribute__((weak)) void process_combo_event(uint16_t combo_index, bool presse
             unregister_code(KC_RSFT);
         }
         break;
-#endif
     case C_BSPC:
         const uint8_t key = get_highest_layer(layer_state) == plt_left_symfunc_index()?
             KC_DEL:
