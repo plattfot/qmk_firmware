5b75c1c3631ee1e02d57927ab6fadf4b7c7b4624
Fredrik Salomonsson <plattfot@posteo.net>
2023-11-10 10:50:14 -0800

    [keymap] plattfot - Update ferris sweep to match mini kyria v4

--- keyboards/ferris/keymaps/plattfot/keymap.c
+++ keyboards/ferris/keymaps/plattfot/keymap.c
@@ -4,11 +4,6 @@
 #include QMK_KEYBOARD_H
 #include "plattfot.h"
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-
 enum layers {
     _BASE,
     _L_SYMFU,
@@ -16,6 +11,8 @@ enum layers {
     _NAV,
     _R_SHORT,
 };
+
+int plt_left_symfunc_index(void) {return _L_SYMFU;}
 
 #define TO_BASE TO(_BASE)
 
@@ -36,28 +33,26 @@ enum combo_events {
   C_CAPS, // caps word
 
   C_CLEAR, // clear state
-  C_LALTL,
-  C_LCTRL,
-  C_LGUI,
   C_LSHIFT,
+  C_LCTRL,
+  C_LALTL,
+  C_LGUI,
 
-  C_CPU, // Ctri + PgUp
-  C_DEL,
-  C_MPLY,
-  C_RALTL,
   C_TAB,
+  C_LNAV, // to navigation
+  C_RALTL,
+  C_MPLY,
 
   C_RSHORT,
-  C_LALTR,
-  C_RCTRL,
-  C_RGUI,
   C_RSHIFT,
+  C_RCTRL,
+  C_LALTR,
+  C_RGUI,
 
-  C_CPD, // Ctri + PgDn
   C_BSPC,
-  C_F20, // mic mute
+  C_RNAV, // to navigation
   C_RALTR,
-  C_NAV, // to navigation
+  C_F20, // mic mute
 
   COMBO_LENGTH
 };
@@ -67,57 +62,52 @@ const uint16_t PROGMEM esc_combo[] = {KC_SPACE, KC_ENT, COMBO_END};
 const uint16_t PROGMEM caps_combo[] = {KC_G, KC_M, COMBO_END};
 
 const uint16_t PROGMEM clear_combo[] = {KC_B, KC_G, COMBO_END};
-const uint16_t PROGMEM laltl_combo[] = {KC_P, KC_T, COMBO_END};
+const uint16_t PROGMEM lshift_combo[] = {KC_P, KC_T, COMBO_END};
 const uint16_t PROGMEM lctrl_combo[] = {KC_F, KC_S, COMBO_END};
-const uint16_t PROGMEM lgui_combo[] = {KC_W, KC_R, COMBO_END};
-const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_A, COMBO_END};
+const uint16_t PROGMEM laltl_combo[] = {KC_W, KC_R, COMBO_END};
+const uint16_t PROGMEM lgui_combo[] = {KC_Q, KC_A, COMBO_END};
 
-const uint16_t PROGMEM cpu_combo[] = {KC_G, KC_V, COMBO_END};
-const uint16_t PROGMEM del_combo[] = {KC_T, KC_D, COMBO_END};
-const uint16_t PROGMEM mply_combo[] = {KC_S, KC_C, COMBO_END};
-const uint16_t PROGMEM raltl_combo[] = {KC_R, KC_X, COMBO_END};
-const uint16_t PROGMEM lshift_combo[] = {KC_A, KC_Z, COMBO_END};
+const uint16_t PROGMEM tab_combo[] = {KC_G, KC_V, COMBO_END};
+const uint16_t PROGMEM lnav_combo[] = {KC_T, KC_D, COMBO_END};
+const uint16_t PROGMEM raltl_combo[] = {KC_S, KC_C, COMBO_END};
+const uint16_t PROGMEM mply_combo[] = {KC_R, KC_X, COMBO_END};
 
 const uint16_t PROGMEM rshort_combo[] = {KC_J, KC_M, COMBO_END};
-const uint16_t PROGMEM laltr_combo[] = {KC_L, KC_N, COMBO_END};
+const uint16_t PROGMEM rshift_combo[] = {KC_L, KC_N, COMBO_END};
 const uint16_t PROGMEM rctrl_combo[] = {KC_U, KC_E, COMBO_END};
-const uint16_t PROGMEM rgui_combo[] = {KC_Y, KC_I, COMBO_END};
-const uint16_t PROGMEM nav_combo[] = {KC_SCLN, KC_O, COMBO_END};
+const uint16_t PROGMEM laltr_combo[] = {KC_Y, KC_I, COMBO_END};
+const uint16_t PROGMEM rgui_combo[] = {KC_SCLN, KC_O, COMBO_END};
 
-const uint16_t PROGMEM cpd_combo[] = {KC_M, KC_K, COMBO_END};
-const uint16_t PROGMEM bspc_combo[] = {KC_N, KC_H, COMBO_END};
-const uint16_t PROGMEM micmute_combo[] = {KC_E, KC_COMM, COMBO_END};
-const uint16_t PROGMEM raltr_combo[] = {KC_I, KC_DOT, COMBO_END};
-const uint16_t PROGMEM rshift_combo[] = {KC_O, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM bspc_combo[] = {KC_M, KC_K, COMBO_END};
+const uint16_t PROGMEM rnav_combo[] = {KC_N, KC_H, COMBO_END};
+const uint16_t PROGMEM raltr_combo[] = {KC_E, KC_COMM, COMBO_END};
+const uint16_t PROGMEM micmute_combo[] = {KC_I, KC_DOT, COMBO_END};
 
 combo_t key_combos[] = {
  [C_ESC] = COMBO_ACTION(esc_combo),
  [C_CAPS] = COMBO_ACTION(caps_combo),
 
  [C_CLEAR] = COMBO_ACTION(clear_combo),
- [C_LALTL] = COMBO_ACTION(laltl_combo),
- [C_LCTRL] = COMBO_ACTION(lctrl_combo),
- [C_LGUI] = COMBO_ACTION(lgui_combo),
  [C_LSHIFT] = COMBO_ACTION(lshift_combo),
+ [C_LCTRL] = COMBO_ACTION(lctrl_combo),
+ [C_LALTL] = COMBO_ACTION(laltl_combo),
+ [C_LGUI] = COMBO_ACTION(lgui_combo),
 
-
- [C_CPU] = COMBO(cpu_combo, LCTL(KC_PGUP)),
- [C_DEL] = COMBO(del_combo, KC_DEL),
- [C_MPLY] = COMBO(mply_combo, KC_MPLY),
- [C_RALTL] = COMBO_ACTION(raltl_combo),
  [C_TAB] = COMBO(tab_combo, KC_TAB),
+ [C_LNAV] = COMBO(lnav_combo, OSL(_NAV)),
+ [C_RALTL] = COMBO_ACTION(raltl_combo),
+ [C_MPLY] = COMBO(mply_combo, KC_MPLY),
 
  [C_RSHORT] = COMBO(rshort_combo, OSL(_R_SHORT)),
- [C_LALTR] = COMBO_ACTION(laltr_combo),
- [C_RCTRL] = COMBO_ACTION(rctrl_combo),
- [C_RGUI] = COMBO_ACTION(rgui_combo),
  [C_RSHIFT] = COMBO_ACTION(rshift_combo),
+ [C_RCTRL] = COMBO_ACTION(rctrl_combo),
+ [C_LALTR] = COMBO_ACTION(laltr_combo),
+ [C_RGUI] = COMBO_ACTION(rgui_combo),
 
- [C_CPD] = COMBO(cpd_combo, RCTL(KC_PGDN)),
- [C_BSPC] = COMBO(bspc_combo, KC_BSPC),
- [C_F20] = COMBO(micmute_combo, KC_F20),
+ [C_BSPC] = COMBO_ACTION(bspc_combo),
+ [C_RNAV] = COMBO(rnav_combo, OSL(_NAV)),
  [C_RALTR] = COMBO_ACTION(raltr_combo),
- [C_NAV] = COMBO(nav_combo, OSL(_NAV)),
+ [C_F20] = COMBO(micmute_combo, KC_F20),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -299,5 +289,18 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
             caps_word_set(!caps_word_get());
         }
         break;
+    case C_BSPC:
+    {
+        const uint8_t key = get_highest_layer(layer_state) == plt_left_symfunc_index()?
+            KC_DEL:
+            KC_BSPC;
+        if (pressed) {
+            register_code(key);
+        } else {
+            unregister_code(key);
+            clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+        }
+        break;
+    }
     }
 }
