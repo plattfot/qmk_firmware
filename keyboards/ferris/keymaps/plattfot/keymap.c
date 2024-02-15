54b9d8bec6ad06610f1230f078c3b84bc0396fbc
Fredrik Salomonsson <plattfot@posteo.net>
2024-02-15 12:47:54 +0100

    [keymap] plattfot - Move F11/F12 and media keys
    - Move F11/F12 to the index finger, to mirror my kyria layer.
    - Move media keys to r_short

--- keyboards/ferris/keymaps/plattfot/keymap.c
+++ keyboards/ferris/keymaps/plattfot/keymap.c
@@ -132,9 +132,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * Left symbol and function Layer
   */
  [_L_SYMFU] = LAYOUT(
-   KC_F12,  KC_F9,   KC_F8,   KC_F7,   KC_VOLU,             KC_LBRC, KC_RBRC, KC_GRV,  KC_DLR,  EU_TDOT,
-   KC_F11,  KC_F3,   KC_F2,   KC_F1,   KC_VOLD,             KC_LCBR, KC_RCBR, KC_QUOT, KC_COLN, KC_CIRC,
-   KC_F10,  KC_F6,   KC_F5,   KC_F4,   CLO_TAP,             KC_LT,   KC_GT,   KC_DQUO, KC_TILDE,KC_QUES,
+   _______,  KC_F9,   KC_F8,   KC_F7,   KC_F12,             KC_LBRC, KC_RBRC, KC_GRV,  KC_DLR,  EU_TDOT,
+   KC_F10,   KC_F3,   KC_F2,   KC_F1,   KC_F11,             KC_LCBR, KC_RCBR, KC_QUOT, KC_COLN, KC_CIRC,
+   _______,  KC_F6,   KC_F5,   KC_F4,   CLO_TAP,            KC_LT,   KC_GT,   KC_DQUO, KC_TILDE,KC_QUES,
                                      TO_BASE, _______, _______, OSL_NAV
 
  ),
@@ -143,8 +143,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * Right symbol and number Layer
   */
  [_R_SYMFU] = LAYOUT(
-   EU_DEG,  KC_AT,   KC_ASTR, KC_HASH, KC_PERC,             KC_MPRV, KC_7,    KC_8,   KC_9,   _______,
-   KC_PIPE, KC_UNDS, KC_MINS, KC_LPRN, KC_RPRN,             KC_MNXT, KC_1,    KC_2,   KC_3,   KC_0,
+   EU_DEG,  KC_AT,   KC_ASTR, KC_HASH, KC_PERC,             _______, KC_7,    KC_8,   KC_9,   _______,
+   KC_PIPE, KC_UNDS, KC_MINS, KC_LPRN, KC_RPRN,             _______, KC_1,    KC_2,   KC_3,   KC_0,
    KC_EXLM, KC_BSLS, KC_PLUS, KC_EQUAL,KC_AMPR,             CLO_TAP, KC_4,    KC_5,   KC_6,   _______,
                                     OSL_NAV, _______, _______, TO_BASE
   ),
@@ -155,13 +155,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_NAV] = LAYOUT(
    _______, _______, KC_RGHT, KC_UP,   KC_LEFT,             _______, _______, _______, _______, _______,
    _______, KC_LEFT, KC_RGHT, KC_DOWN, _______,             _______, KC_DOWN, KC_HOME, KC_END,  _______,
-   _______, _______, CTL_PUP, CTL_PDN,_______,              _______, _______, KC_PGUP, KC_PGDN, _______,
+   _______, _______, CTL_PUP, CTL_PDN, _______,             _______, _______, KC_PGUP, KC_PGDN, _______,
                                     TO_BASE, _______, _______, TO_BASE
  ),
 
  [_R_SHORT] = LAYOUT(
-    _______, _______, _______, _______, _______,             _______, G(KC_7), G(KC_8), G(KC_9), _______,
-    _______, _______, _______, _______, _______,             _______, G(KC_1), G(KC_2), G(KC_3), G(KC_0),
+    _______, _______, KC_MPRV, KC_MNXT, _______,             _______, G(KC_7), G(KC_8), G(KC_9), _______,
+    _______, _______, KC_VOLD, KC_VOLU, _______,             _______, G(KC_1), G(KC_2), G(KC_3), G(KC_0),
     _______, _______, _______, _______, _______,             _______, G(KC_4), G(KC_5), G(KC_6), _______,
                                      _______, _______, _______, _______
 
